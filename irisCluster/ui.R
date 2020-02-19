2536a3c6698a8a55bfd02bf3bec2b1a549793d6e
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-02-19 00:18:07 -0600

    tutorials - two separate files
    https://shiny.rstudio.com/gallery/kmeans-example.html

---
+++ irisCluster/ui.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+
+pageWithSidebar(
+    headerPanel('Iris k-means clustering'),
+    sidebarPanel(
+        selectInput('xcol', 'X Variable', names(iris)),
+        selectInput('ycol', 'Y Variable', names(iris),
+                    selected=names(iris)[[2]]),
+        numericInput('clusters', 'Cluster count', 3,
+                     min = 1, max = 9)
+    ),
+    mainPanel(
+        plotOutput('plot1')
+    )
+)
+
+
